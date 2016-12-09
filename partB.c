d321f69cbd5413cb96b4d7cfb701f3d4b73671e0
Parallels <brendanwflucas@gmail.com>
2016-12-09 15:23:22 -0500

    Fully working DB edditor

--- partB.c
+++ partB.c
@@ -1,58 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include <stdin.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-#include <semaphore.h>
 #include <sys/msg.h>
 #include <sys/types.h>
 #include <string.h>
 #include <pthread.h>
 #include <stdbool.h>
-//#include <mqueue.h>
-
-#define SEM_MODE (0400 | 0200 | 044)
-#define SHM_MODE (SHM_R | SHM_W | 044)
-
-
-union semun {
-	int val;
-	struct semid_ds *buf;
-	short * array;
-} arg ;
 
 void *atmfunc(void *msqid);
 void *dbServer(void *msqid);
-void *dbEditor(void);
+void *dbEditor(void *zero);
 void getInput(char *question, char *returnString, int inputLength);
-//db edditor declared
-int SemaphoreWait(int semid, int iMayBlock );
-int SemaphoreSignal(int semid);
-void SemaphoreRemove(int semid);
-int SemaphoreCreate(int iInitialValue);
+char* getFeild(char* line, int n, char c);
 
-
-int shmId;
-int semID;
-
-int uaccnum;
-int upin;
-
-
-//////// This is the DATA BASE //////////
-typedef struct {
+//////// DATA BASE Entrys //////////
+typedef struct data_base_entry{
   char   accnum[6];
   double fundsav;
 	char   pin[4];
 
 } DB;
 
-DB* shared;///this is a shared variable of type DB
-
-
-//this is the first buffer that will be transferd between ATM and DB server
+//message truct for sending between atm and  DBserver.
 typedef struct my_msgbuf {
     long    msgtype;
     struct msgContents{
@@ -65,15 +37,14 @@ typedef struct my_msgbuf {
 		}contents;
 }my_msgbuf;
 
+//Globals
+//constants
 #define NUMoTHREADs 3
 
+pthread_t threads[NUMoTHREADs];
+///this is a shared variable of type DB//TODO: get rid of this one;
 ///////////////______MAIN_____////////////////////////
 int main(){
-
-  pthread_t threads[NUMoTHREADs];
-  //Test variables:
-  char* ACCOUNT= "00342";
-  char* PIN = "123";
 	///////// Message Queue creation///////
 	int msqid;
 	int msgtype;
@@ -82,28 +53,13 @@ int main(){
   my_msgbuf sbuf,rbuf;
   size_t buf_length;
 	key=1234;
-	if ((msqid =(int)msgget(key, msgflg)) < 0) {
+	if ((msqid = msgget(key, msgflg)) < 0) {
         		perror("msgget");
         		exit(1);
     }else{
 				(void)fprintf(stderr,"msgget: msgget succeeded: msqid = %i\n", msqid);
 	}
-
-	semID=SemaphoreCreate(1);
-	shmId = shmget(IPC_PRIVATE, 100 * sizeof(DB) , SHM_MODE);
-	if((shmat(shmId, (DB*)0, 0))  ==(int*)-1){
-		printf("Error allocating memory\n");
-	}
-	shared = (DB*) shmat(shmId, (DB*)0,0);
-
-  //Initialize shared memory
-	strcpy(shared[0].accnum, "00123");
-	strcpy(shared[0].pin, "001");
-	shared[0].fundsav = 123.7;
-
-  for(int i=3;i<100;i++){
-      strcpy(shared[i].accnum, "00000");
-  }
+  /*
 	int rc=0;
   if(rc=pthread_create(&threads[0], NULL, atmfunc, (void *)(long)msqid)){
 			printf("ERROR: pthread 0 supposed to return 0\n");
@@ -113,12 +69,18 @@ int main(){
 	}
 
   if(pthread_create(&threads[1], NULL, dbServer, (void *)(long)msqid)){
-      printf("ERROR: pthread 1 supposed to return 0\n");
-      exit(1);
+			printf("ERROR: pthread 1 supposed to return 0\n");
+			exit(1);
+	}
+  */
+	if(pthread_create(&threads[2], NULL, dbEditor, (void *)0)){
+			printf("ERROR: pthread 2 supposed to return 0\n");
+			exit(1);
 	}
 
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
+	//pthread_join(threads[0], NULL);
+	//pthread_join(threads[1], NULL);
+	pthread_join(threads[2], NULL);
 	printf("exiting main\n");
   pthread_exit(NULL);
   return 0;
@@ -176,7 +138,7 @@ void *atmfunc(void *msq){
 	        }
 	        sbuf.contents.uchoice=uchoice1;
 
-	        if(uchoice1==2){//Withdraw ammount
+	        if(uchoice1==2){//Withdraw amount
 	            printf("please enter the amount you would like to withdraw: \n");
 	            scanf("%lf", &sbuf.contents.uwithdraw);
 	        }
@@ -213,13 +175,24 @@ void *dbServer(void *msq){
     int msqid = (int)(long)msq;
     int msgLength = sizeof(rbuf.contents);
 		int i=0;
-		//TODO: set stat to 3 if not enough funds in server
+		rbuf.contents.uchoice=0;
+		sbuf.contents.stat=0;
+    DB shared[100];
+    FILE* io = fopen("database.txt", "r+");
+    char line[1024];
+    int sharedSize=0;
+    //fill shared data array.
+    while(fgets(line, 1024, io)){
+        strcpy(shared[sharedSize].accnum, getFeild(line,  1, ','));
+        strcpy(shared[sharedSize].pin, getFeild(line, 2, ','));
+        shared[sharedSize++].fundsav = atof(getFeild(line, 3, ','));
+    }
 		if (msgrcv(msqid, &rbuf, msgLength, fromATM, 0) < 0) { //what is message type, I wrote it accnum in this case
 				perror("msgrcv");
 				exit(1);
 		}
 		printf("Server: message recieved \n");
-		for(i=0; i<100; i++){
+		for(i=0; i<sharedSize; i++){
 				printf("Server: account number is %s, while account number #%d is %s \n", rbuf.contents.uaccnum, i, shared[i].accnum);
         if(strcmp(rbuf.contents.uaccnum, shared[i].accnum)==0){
 						printf("Server: first if reached in accnumber= %s..\n", shared[i].accnum);
@@ -244,7 +217,7 @@ void *dbServer(void *msq){
 		                    perror("msgsnd");
 		                    exit(1);
 		                }
-		            }else if(rbuf.contents.uchoice==2){//user chose to withdraw ammount
+		            }else if(rbuf.contents.uchoice==2){//user chose to withdraw amount
 		            		if(rbuf.contents.uwithdraw<=shared[i].fundsav){
 		                		shared[i].fundsav = shared[i].fundsav - rbuf.contents.uwithdraw;
 		                		sbuf.contents.ufundsav = shared[i].fundsav;
@@ -281,46 +254,157 @@ void *dbServer(void *msq){
 						exit(1);
 				}
 		}
+		printf("Server: exiting\n");
 };
-/*
 
-void *dbEditor(){
-
-
-    char newpin[3];
+void *dbEditor(void *zero){
+    char newpin[4];
     double amount;
-    char newaccnum[5];
-    char empty[5]={ 0, 0, 0, 0, 0 };
-    printf("%s",empty);
+    char newaccnum[6];
+		bool found = false;
+		char decision=0x00;
+		char line[1024];
+    int accnumLine=0;
+		char lines[100][50];
+    FILE* io;
+		char *initialArr[4] = {"00001,107,3443.22,\n", "00011,323,10089.97,\n", "00117,259,112.00,\n", NULL};
+		printf("enteredDBEditor\n");
+		printf("would you like to set the database back to default? [Y|n]\n");
+		scanf("%c", &decision);
+		if (decision == 'y' || decision == 'Y'){
+	      io =  fopen("database.txt", "w+");
+				for (int i=0; i<3; i++) {
+						printf("in for %i\n", i);
+						fputs(initialArr[i], io);
+				}
+        fclose(io);
+				printf("reset file\n");
+		}
+		io =  fopen("database.txt", "r+");
+
+    decision=0x00;
+		printf("oppenned File data\n");
+		printf("Make changes to database?? [Y|n]\n");
+		//scanf("%c", &decision);
+    decision='y';
+		if (decision == 'y' || decision == 'Y'){
+				printf("enter an account number, if it exiests you will be asked to delete or edit it\n");
+				printf("if it does not, it will be added and you will be asked to edit its specifics\n");
+				scanf("%s", newaccnum);
+				int lncount=0;
+				found = false;
+				while(fgets(line, 1024, io)){
+						strcpy(lines[lncount],line);
+						if (strcmp(getFeild(line, 1, ','), newaccnum)==0){
+								found = true;
+								accnumLine = lncount;
+						}
+						lncount++;
+				}
+        for(int i=0; i<lncount; i++)printf("line at %i = %s\n", i, lines[i]);
+				if(found){
+						printf("Account number found would you like to delete or edit?[d|e]\n");
+            scanf("%c", &decision);
+            if(decision == 'd' || decision == 'D'){
+                //REWRITE
+                fclose(io);
+                io = fopen("database.txt", "w+");
+                for (int i=0; i<lncount; i++){
+                    if(i!=accnumLine)fputs(lines[i], io);
+                }
+            }else {
+                printf("would you like to change the account ballance or the pin? [b/p]\n");
+                scanf("%c", &decision);
+                if(decision == 'b' || decision == 'B'){
+                    printf("What would you like to change the balance to?\n");
+                    scanf("%lf", &amount);
+                    char *tempLine[3];
+                    for(int i=1; i<4; i++){
+                        tempLine[i-1] = getFeild(lines[accnumLine], i, ',');
+                    }
+                    sprintf(tempLine[2], "%lf", amount);
+                    strcpy(lines[accnumLine], ("%s,%s,%s,", tempLine[0],tempLine[1], tempLine[2]));
+                    //REWRITE
+                    fclose(io);
+                    io = fopen("database.txt", "w+");
+                    for (int i=0; i<lncount; i++){
+                        fputs(lines[i], io);
+                    }
+                }else{
+                    printf("What would you like to change the PIN to?\n");
+                    scanf("%s", newpin);
+                    char *tempLine[3];
+                    for(int i=1; i<4; i++){
+                        tempLine[i-1] = getFeild(lines[accnumLine], i, ',');
+                    }
+                    strcpy(tempLine[1], newpin);
+                    strcpy(lines[accnumLine], ("%s,%s,%s,", tempLine[0],tempLine[1], tempLine[2]));
+                    //REWRITE
+                    fclose(io);
+                    io = fopen("database.txt", "w+");
+                    for (int i=0; i<lncount; i++){
+                        fputs(lines[i], io);
+                    }
+                }
+            }
+				}else {
+            accnumLine=lncount;
+            lncount++;
+            char *amountStr;
+            char temp;
+            printf("adding account number to database\n");
+            printf("Please enter the pin for the account (3 characters please):\n");
+            scanf("%c", &temp);
+            for (int i=0; i<3; i++) scanf("%c", &newpin[i]);
+            printf("Please enter the new account balance\n");
+            //scanf("%s", amountStr);
+            amount=90.9;
+            printf("%f\n", amount);
+            amountStr = "90.90";
+            //sprintf(amountStr, "%lf", amount);
+            printf("converted double to string\n");
+            printf("%s\n", newaccnum);
+            printf("%s\n", newpin);
+
+            strcpy(lines[accnumLine],  newaccnum);
+            strcat(lines[accnumLine],",");
+            strcat(lines[accnumLine], newpin);
+            strcat(lines[accnumLine],",");
+            strcat(lines[accnumLine],  amountStr);
+            strcat(lines[accnumLine],",\n");
+            printf("%s\n", lines[accnumLine]);
+            printf("Printing new line to file\n");
+            fclose(io);
+            io=fopen("database.txt", "w+");
+            for (int i=0; i<lncount; i++){
+                fputs(lines[i], io);
+            }
+            fclose(io);
+            printf("exiting last part of edit\n");
+        }
+		}
+    printf("exiting database editor\n");
     // int z =10425;
     //  while(z=10425){ no need for a while true or it will be stuck forever
     //we need to put a defult case ==> every empty slot in the array of 100 elemnt has an accnum of 00000
-        for(int i=0;i<100;i++){
-            if(!strcmp(shared[i].accnum, empty)){ //since it returns zero if they are equivalent
-
-                printf("Enter New Account Number: \n");
-                scanf("%s",newaccnum);
-                strcpy(shared[i].accnum,newaccnum);
-                printf("Enter new pin");
-                scanf("%i",newpin);
-                strcpy(shared[i].pin,newpin);
-                printf("enter the amount of the deposite/withdraw");
-                scanf("%f",amount);
-                shared[i].fundsav= shared[i].fundsav - amount;
-
-                //send update to dbserver "HOW !??"
-               // shared+=sizeof(DB);
-            }else{
-
-                printf("NO MORE SPACE FOR A NEW COSTUMER");
-
-            }
+    /*for(int i=0;i<100;i++){
+       if(strcmp(shared[i].accnum, "00000")==0){ //since it returns zero if they are equivalent
+            printf("Enter New Account Number: \n");
+            scanf("%s",newaccnum);
+            strcpy(shared[i].accnum, newaccnum);
+            printf("Enter new pin\n");
+            scanf("%s", newpin);
+            strcpy(shared[i].pin, newpin);
+            printf("enter the amount of the deposite/withdraw\n");
+            scanf("%lf", &amount);
+            shared[i].fundsav = shared[i].fundsav - amount;
+           	// shared+=sizeof(DB);
+        }else{
+            printf("NO MORE SPACE FOR A NEW COSTUMER");
         }
+    }*/
    // }
-
-
 }
-*/
 
 void getInput(char *question, char *returnString, int inputLength){
 		printf("%s *you may only use %d charavters:\n", question, inputLength);
@@ -328,58 +412,13 @@ void getInput(char *question, char *returnString, int inputLength){
 		printf("returning\n");
 };
 
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////
-////////////////// Semaphore functions////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-
-
-int SemaphoreWait(int semid, int iMayBlock ) {
-struct sembuf
-sbOperation;
-sbOperation.sem_num = 0;
-sbOperation.sem_op = -1;
-sbOperation.sem_flg = iMayBlock;
-return semop( semid, &sbOperation, 1 );
+char* getFeild(char* line, int n, char c){
+		char* tok;
+		for (tok = strtok(line, &c); tok && *tok; tok = strtok(NULL, ("%c\n",&c))){
+				if (!--n)
+						return tok;
+		}
+		return NULL;
 }
 
-
-int SemaphoreSignal( int semid ) {struct sembuf
-sbOperation;
-sbOperation.sem_num = 0;
-sbOperation.sem_op = +1;
-sbOperation.sem_flg = 0;
-return semop( semid, &sbOperation, 1 );
-}
-
-
-void SemaphoreRemove( int semid ) {
-if(semid != -1 )
-semctl( semid, 0, IPC_RMID , 0);
-}
-
-
-int SemaphoreCreate(int iInitialValue) {
-int semid;
-union semun suInitData;
-int iError;
-/* get a semaphore */
-semid = semget( IPC_PRIVATE, 1, SEM_MODE );
-/* check for errors */
-if( semid == -1)
-return semid;
-/* now initialize the semaphore */
-suInitData.val = iInitialValue;
-if(semctl( semid, 0, SETVAL, suInitData) == -1 )
-{ /* error occurred, so remove semaphore */
-
-SemaphoreRemove(semid);
-return -1;
-}
-return semid;
-}
+//void rewrite()
